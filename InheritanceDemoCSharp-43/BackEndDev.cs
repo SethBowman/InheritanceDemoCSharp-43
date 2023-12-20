49f11d64e745c77f9706e758de78e43f680a54b1
Seth Bowman <brandonsethbowman@gmail.com>
2023-12-20 10:14:21 -0600

    Add project files.

---
+++ InheritanceDemoCSharp-43/BackEndDev.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemoCSharp_43
+{
+    public class BackEndDev : Coder
+    {
+    }
+}
---
+++ InheritanceDemoCSharp-43/Program.cs
@@ -0,0 +1,23 @@
+﻿using InheritanceDemoCSharp_43;
+
+
+var seth = new Person();
+seth.Name = "Seth";
+seth.Age = 29;
+
+seth.Greet();
+
+var mack = new Coder();
+mack.Name = "Mack";
+mack.Age = 24;
+mack.ProgrammingLang = "C#";
+mack.Greet();
+mack.Code();
+
+var cruz = new BackEndDev();
+
+cruz.Name = "Cruz";
+cruz.Age = 32;
+cruz.ProgrammingLang = "JavaScript";
+cruz.Greet();
+cruz.Code();
